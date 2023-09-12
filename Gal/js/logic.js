225e468150343d1ef414c35c00e0f287069b08df
Kokolipa <galbeeri1@gmail.com>
2023-09-12 21:38:33 +1000

    Adding the JS file and modifying the code for the JS file

--- Gal/js/logic.js
+++ Gal/js/logic.js
@@ -1,11 +1,41 @@
 // ! Subsets Variables
 // ! #################################################################
-
+let selectedDate;
+let selectedEndDate;
 
 
 // ! Creating Functions - Functions Funnel
 // ! #################################################################
+document.addEventListener("DOMContentLoaded", function() {
+    let dateInput = document.getElementById("dateRange_initial");
+    let endDateInput = document.getElementById("dateRange_end"); 
+  
+    dateInput.addEventListener("change", function() {
+      // Get the selected date value from the input element
+      selectedDate = dateInput.value;
+      
+      if (!endDateInput.value){ 
+        alert("Please select End date");
+      } else {
+        console.log("Start Date: " + selectedDate, "End Date: " + endDateInput.value);
+      }
+    });
+  
+    endDateInput.addEventListener("change", function() {
+      // Get the selected date value from the input element
+      selectedEndDate = endDateInput.value;
+      
+      if (!dateInput.value){
+        alert("Please select Start date");
+      } else {
+        console.log("Start Date: " + dateInput.value, "End Date: " + selectedEndDate);
+      }
+    });
+  });
+  
 
+// * Create a function to retreive the start date & end date --> The function will filter the data based on the date range
+    // * The second condition will be that if user only applied one date object (start/end date) then the output be "please select End date/Start Date" AS PRORMPT
 
 // USING D3 TO READ JSON DATA -- DATA FUNNEL
 // #################################################################
@@ -15,6 +45,7 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 d3.json(url).then(function(data) {
     // * confirming the data was migrated correctly
     console.log(data);
+    console.log("Start Date: " + selectedDate, "End Date: " + selectedEndDate);
 }).catch(function(error) {
     console.log('Error loading the JSON file: ' + error);
 });
