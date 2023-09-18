e506e8a454d2d45539eafa05026c72eacba3669c
anna2023471 <russiaanna@yahoo.co.nz>
2023-09-19 11:17:39 +1200

    branch tidied up

--- flask_sqlite_js/routes/get_data_by_date.py
+++ flask_sqlite_js/routes/get_data_by_date.py
@@ -2,24 +2,27 @@ from flask import jsonify
 import sqlite3
 from datetime import datetime
 
-DATABASE_PATH = "./nasdaq.sqlite"
+DATABASE_PATH = ".././nasdaq.sqlite"
 
-# Example: http://localhost:5000/sector/Technology
-def get_data_by_sector_route(app):
-    @app.route("/sector/<string:sector>")
-    def get_data_by_sector(sector):
+# Example: http://localhost:5000/date/01-01-2020/31-12-2020
+def get_data_by_date_route(app):
+    @app.route("/date/<string:start_date>/<string:end_date>")
+    def get_data_by_date(start_date, end_date):
         # Connect to the SQLite database
         conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
-
-        sector = sector.upper()
         
         # Lists to store the final data
         tickers = []
         metadata_list = []
         trades_list = []
         
-        # Fetch tickers and metadata for the specified sector
+        # Define date variables
+        start_date_db = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date_db = datetime.strptime(end_date, '%Y-%m-%d')
+        
+        
+        # Fetch tickers and metadata
         cursor.execute("""SELECT ticker_id, symbol, asset_type, name, description, currency, country, sector, industry, market_capitalization, ebitda, book_value, dividend_per_share, dividend_yield 
                        FROM metadata
                        JOIN sector s
@@ -31,11 +34,8 @@ def get_data_by_sector_route(app):
                        JOIN country co
                        ON metadata.country_id = co.country_id
                        JOIN currency cu
-                       ON metadata.currency_id = cu.currency_id WHERE sector=?""", (sector,))
+                       ON metadata.currency_id = cu.currency_id""")
         metadata_rows = cursor.fetchall()
-        
-        if not metadata_rows:
-            return jsonify({"error": "Sector not found"}), 404
         
         for row in metadata_rows:
             ticker = row[0]
@@ -54,8 +54,8 @@ def get_data_by_sector_route(app):
                 "industry": row[8]
             })
             
-            # Fetch trade data for the current ticker
-            cursor.execute("SELECT date, open, high, low, close, volume FROM data WHERE ticker_id=?", (ticker,))
+            # Fetch trade data for the current ticker within the specified date range
+            cursor.execute("SELECT date, open, high, low, close, volume FROM data WHERE ticker_id=? AND date BETWEEN ? AND ?", (ticker, start_date_db, end_date_db))
             trade_rows = cursor.fetchall()
             
             dates = []
@@ -66,10 +66,8 @@ def get_data_by_sector_route(app):
             volumes = []
             
             for trade in trade_rows:
-                # Format the date to 'dd-mm-yyyy'
-                formatted_date = datetime.strptime(trade[0], '%Y-%m-%d').strftime('%d-%m-%Y')
                 
-                dates.append(formatted_date)
+                dates.append(trade[0])
                 open_prices.append(trade[1])
                 high_prices.append(trade[2])
                 low_prices.append(trade[3])
